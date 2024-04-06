d5bc65584e1676e75880af65167c858142c63ba0
Hammad-Ali07 <hammii272878@gmail.com>
2024-04-05 19:58:42 -0700

    add files

---
+++ game.ts
@@ -0,0 +1,21 @@
+
+import inquirer from "inquirer";
+let randomNumber = Math.floor(Math.random() * 10+1);
+
+
+
+let guessingNumber = await inquirer.prompt([
+  {
+    message: "Enter Your Number Between 1-10:",
+    type: "number",
+    name: "YourGuess",
+  },
+]);
+
+//conditional test
+if(guessingNumber.YourGuess === randomNumber) {
+  console.log("congtratulations! You Guess Is Right..........." );
+  
+}else{
+  console.log("Oops! You Guess Is Wrong....")
+}
